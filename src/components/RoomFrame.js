379ef1fd8b42144a1b5073ec69b0a933ad257c80
Mustafiz Ansari <mustafizansari35@gmail.com>
2024-03-01 03:37:15 +0530

    image and result with appi

--- src/components/RoomFrame.js
+++ src/components/RoomFrame.js
@@ -1,13 +1,39 @@
 import { useRef, useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
-import { v4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
+
+//firebase Imports
+import db, { storage } from '../firbaseConfig';
+import { getDownloadURL, ref, uploadString } from 'firebase/storage';
+import { push, set } from 'firebase/database';
 
 const RoomFrame = () => {
     const { roomId } = useParams();
-    const [images, setImages] = useState([]);
+    const [imageData, setImageData] = useState([]);
+    const [chatMessages, setChatMessages] = useState([]);
+    const [startApiCall, setStartApiCall] = useState(false)
+    const navigate = useNavigate();
 
-    const captureImage = () => {
+
+    // Function to test the connection by writing data to the database
+    // const testConnection = async () => {
+    //     try {
+    //         const dataRef = ref(db, 'testData');
+    //         await set(dataRef, 'Hello, Firebase!');
+    //         console.log('Data written successfully.');
+    //     } catch (error) {
+    //         console.error('Error writing data:', error);
+    //     }
+    // };
+    // testConnection();
+
+    const [isCapturing, setIsCapturing] = useState(true);
+
+
+
+    const captureImage = async () => {
         const mainFrame = document.querySelector('.mainFrame');
         if (mainFrame) {
             const videoElement = mainFrame.querySelector('video');
@@ -25,11 +51,94 @@ const RoomFrame = () => {
                 // Convert canvas to base64 data URL
                 const imageDataURL = canvas.toDataURL('image/jpeg');
 
+                try {
+                    //Get the Random ID of Student
+                    const studentId = Math.floor(Math.random() * 10) + 1;
+
+                    // Construct the image path with student ID
+                    const studentImagePath = `students/${studentId}`;
+
+                    // Generate a unique image name using the current timestamp
+                    const timestamp = Date.now(); // Current timestamp in milliseconds
+                    const imageName = `${studentId}_${timestamp}.jpg`;
+
+                    // Upload image to Firebase Storage with the constructed image path and name
+                    const imageRef = ref(storage, `${studentImagePath}/${imageName}`);
+                    await uploadString(imageRef, imageDataURL, 'data_url');
+
+                    // Get the download URL for the uploaded image
+                    const downloadURL = await getDownloadURL(imageRef);
+
+
+                    // Add image data to state
+                    setImageData(prevImageData => [
+                        ...prevImageData,
+                        { studentPID: studentId, imageUrl: downloadURL }
+                    ]);
+
+
+                } catch (error) {
+                    console.error('Error uploading image:', error);
+                }
                 // Add image data URL to the images array
-                setImages(prevImages => [...prevImages, imageDataURL]);
+                // setImages(prevImages => [...prevImages, imageDataURL]);
             }
         }
     };
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (isCapturing) {
+                captureImage();
+            }
+        }, 3000); // Capture image every 3 seconds
+
+        if (isCapturing) {
+            setTimeout(() => {
+                setIsCapturing(false); // Pause capturing during emotion detection
+                emotionDetect().then(() => {
+                    setIsCapturing(true); // Resume capturing after emotion detection
+                });
+            }, 15000); // Wait for 15 seconds before emotion detection
+        }
+
+        // Cleanup interval
+        return () => clearInterval(interval);
+    }, [isCapturing]); // Re-run effect when 'isCapturing' changes
+
+    const emotionDetect = async () => {
+        console.log('Detecting .....');
+
+        try {
+            const response = await axios.post('https://mood-lens-server.onrender.com/api/predict_gemini/images_to_emotions', {
+                imgUrls: imageData.slice(-5)
+            });
+
+            console.log('Response from API:', response.data);
+        } catch (error) {
+            console.log('error in api calling', error.message);
+
+        }
+
+
+    };
+
+
+
+
+    // useEffect(() => {
+    //     if (imageData.length > 5) {
+
+    //         setTimeout(emotionDetect, 8000);
+
+
+    //     }
+    // }, [imageData]);
+
+
+
+
+
 
 
 
@@ -41,7 +150,7 @@ const RoomFrame = () => {
             appID,
             serverSecret,
             roomId,
-            v4(),
+            uuidv4(),
             'Your Name'
         );
         if (!appID || !serverSecret) {
@@ -61,31 +170,43 @@ const RoomFrame = () => {
             scenario: {
                 mode: ZegoUIKitPrebuilt.VideoConference,
             },
-            onJoinRoom: () => {
-                // Start capturing images once the video starts playing
-                setInterval(captureImage, 4000);
+            onJoinRoom: async () => {
+                // setStartApiCall(true)
+
+
+                // console.log('Calling captureImage...');
+                // const captureImageTimeout = setInterval(() => {
+                //     captureImage();
+                // }, 5000); // Capture image every 3 seconds
+
+            },
+            onInRoomMessageReceived: (data) => {
+                setChatMessages(prevMessages => [...prevMessages, data.message]);
             }
         });
     };
-    console.log(images);
+
+    console.log(imageData);
+    // console.log(chatMessages);
 
 
 
     return (
         <>
-            <div className="mainFrame" ref={meetingUI}>
+            <div className="mainFrame" ref={meetingUI} style={{ width: '100%', height: '100vh' }} >
             </div>
 
-            <div className='image-display p-5 border'>
+            {/* <div className='image-display p-5 border'>
                 <h2>Captured Images:</h2>
                 <div className='border border-info p-3'>
                     {images.map((image, index) => (
                         <img key={index} src={image} alt={`Captured Image ${index}`} className='m-2 p-3 border  border-success' />
                     ))}
                 </div>
-            </div>
+            </div> */}
         </>
     );
+
 };
 
 export default RoomFrame;
---
+++ src/firbaseConfig.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getStorage } from 'firebase/storage';
+import { getDatabase } from 'firebase/database';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+// Initialize Firebase app
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Get a reference to the Firebase Realtime Database
+const db = getDatabase(firebaseApp);
+export const storage = getStorage(firebaseApp);
+
+export default db;
+
